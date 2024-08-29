a293d70194a9acedd44fcdedd0ddb5b721c18d32
mpqm <okqkrwhdtjd@gmail.com>
2024-08-30 01:36:07 +0900

    test frontend-dev hotfix push!

--- backend/src/main/java/com/sabujaks/backend/TestController.java
+++ backend/src/main/java/com/sabujaks/backend/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("backend/feature/post\n");
+        return ResponseEntity.ok("hotfix!\n");
     }
 }
