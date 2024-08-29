5e1c4b77f7fcb4794b866c68ca1c29950d50e97c
mpqm <okqkrwhdtjd@gmail.com>
2024-08-30 00:57:23 +0900

    test start

---
+++ backend/src/main/java/com/sabujaks/backend/TestController.java
@@ -0,0 +1,13 @@
+package com.sabujaks.backend;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("버전 업그레이드 완료\n");
+    }
+}
---
+++ backend/src/test/java/com/sabujaks/backend/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabujaks.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
